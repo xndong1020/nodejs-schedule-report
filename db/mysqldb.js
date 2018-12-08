8bfe6d06661fea2cd3cdeca6de60743c663cffc7
Jeremy Gu <isdance2004@hotmail.com>
2018-12-09 01:13:07 +1100

    add mysql for saving tasks

---
+++ db/mysqldb.js
@@ -0,0 +1,12 @@
+// get the client
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "den1.mysql3.gear.host",
+  user: "schedulereport",
+  password: "Hy2QN-EfJ_R3",
+  database: "schedulereport"
+});
+
+exports.db = connection;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const { io } = require("../io");
+const { createTaskAsync, updateTaskAsync } = require("../MysqlDBHelpers");
 
-router.post("/", (req, res) => {
-  const task = req.body;
-  io.sockets.emit("newTask", task);
-  res.send(task);
+router.post("/", async (req, res) => {
+  const newTask = req.body;
+  const { task_id } = newTask;
+  let result = undefined,
+    isSqlSuccessful = undefined,
+    task = {};
+
+  if (!task_id) {
+    result = await createTaskAsync(newTask);
+    isSqlSuccessful = result.affectedRows === 1 && result.insertId > 0;
+    task = { ...newTask, task_id: result.insertId };
+  } else {
+    result = await updateTaskAsync(newTask);
+    isSqlSuccessful = result.affectedRows === 1 && result.changedRows > 0;
+    task = { ...newTask };
+  }
+
+  if (isSqlSuccessful) {
+    io.sockets.emit("newTask", task);
+    res.status(200).send(task);
+  } else {
+    res.status(500).send("Failed to create task");
+  }
 });
 
 module.exports = router;
