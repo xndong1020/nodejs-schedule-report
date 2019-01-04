0c62f74c43847898278f53aece7cac392898cf67
Jeremy Gu <isdance2004@hotmail.com>
2019-01-05 07:34:02 +1100

    add env file

--- db/index.js
+++ db/index.js
@@ -1,19 +1,16 @@
-//Import the mongoose module
 const mongoose = require("mongoose");
 const bluebird = require("bluebird");
-const { config } = require("../config");
-//Set up default mongoose connection
-mongoose.connect(
-  config.mongodb_url,
-  { useNewUrlParser: true }
-);
+require("dotenv").config(); // to read value of process.env.MongoURI
+
+mongoose
+  .connect(
+    process.env.MONGODB_URL,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Mongodb connected"))
+  .catch(err => console.log(err));
+
 // Get Mongoose to use the bluebird promise library
 mongoose.Promise = bluebird;
-//Get the default connection
-var db = mongoose.connection;
 
-//Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.on("connected", console.log.bind(console, "MongoDB connected."));
-
-module.exports = db;
+module.exports = mongoose;
