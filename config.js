385640673ee76853a1ec7ecd3186f9fcd27adc1e
Jeremy Gu <isdance2004@hotmail.com>
2019-01-04 16:50:05 +1100

    remove mysql save

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ exports.config = {
       "mongodb://admin:password2018@ds227664.mlab.com:27664/nodejs-schedule-jobs",
     socketio_server_url: "http://localhost:4000",
     username: "jeremyDev",
-    password: "******",
+    password: "Cc51315704",
     call_wait_time: 30000,
     repeat_call: 3,
     email_username: "isdance2004.yg@gmail.com",
-    email_password: "*******"
+    email_password: "Cc51315704"
   };
   
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,31 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { io } = require("../io");
-const { createTaskAsync, updateTaskAsync } = require("../MysqlDBHelpers");
+// const { createTaskAsync, updateTaskAsync } = require("../MysqlDBHelpers");
 
 router.post("/", async (req, res) => {
   const newTask = req.body;
-  const { task_id } = newTask;
-  let result = undefined,
-    isSqlSuccessful = undefined,
-    task = {};
-
-  if (!task_id) {
-    result = await createTaskAsync(newTask);
-    isSqlSuccessful = result.affectedRows === 1 && result.insertId > 0;
-    task = { ...newTask, task_id: result.insertId };
-  } else {
-    result = await updateTaskAsync(newTask);
-    isSqlSuccessful = result.affectedRows === 1 && result.changedRows > 0;
-    task = { ...newTask };
-  }
-
-  if (isSqlSuccessful) {
-    io.sockets.emit("newTask", task);
-    res.status(200).send(task);
-  } else {
-    res.status(500).send("Failed to create task");
-  }
+  io.sockets.emit("newTask", newTask);
+  res.status(200).send(newTask);
 });
 
 module.exports = router;
