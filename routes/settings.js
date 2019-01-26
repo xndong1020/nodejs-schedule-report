597c7452f221c6ba15e1057bf4781680253e9983
Jeremy Gu <isdance2004@hotmail.com>
2019-01-26 22:50:27 +1100

    add delete device

--- routes/settings.js
+++ routes/settings.js
@@ -7,7 +7,7 @@ const { getDeviceSettingsByUserID } = require('../mongodbHelpers')
 // GET /settings/add_device
 router.get('/add_device', async (req, res) => {
   const { _id } = req.user
-  const settings = await getDeviceSettingsByUserID(_id) || {}
+  const settings = (await getDeviceSettingsByUserID(_id)) || {}
   const { name } = req.user
 
   const deviceKeys = Object.keys(deviceTypes)
@@ -28,8 +28,9 @@ router.get('/add_device', async (req, res) => {
 // GET /settings/admin_devices
 router.get('/admin_devices', async (req, res) => {
   const { _id } = req.user
-  const settings = await getDeviceSettingsByUserID(_id) || {}
+  const settings = (await getDeviceSettingsByUserID(_id)) || {}
   const { devices } = settings
+  const deviceId = settings._id
   const { name } = req.user
 
   const deviceKeys = Object.keys(deviceTypes)
@@ -38,6 +39,7 @@ router.get('/admin_devices', async (req, res) => {
   res.render('admin_devices', {
     title: 'Devices',
     name,
+    deviceId,
     devices,
     deviceKeys
   })
@@ -52,6 +54,31 @@ router.post('/add_device', (req, res) => {
   req.flash('success_msg', 'Your device has been saved for testing')
   // read user settings
   res.redirect('/')
+})
+
+// POST /settings/add_device
+router.post('/:deviceId/delete_device/:role', async (req, res) => {
+  const { deviceId, role } = req.params
+  try {
+    const targetDeviceObj = await Device.findOne({ _id: deviceId })
+    const { devices } = targetDeviceObj
+    const newDeviceList = devices.filter(device => device.role !== role)
+    const newDeviceObj = {
+      _id: deviceId,
+      devices: newDeviceList,
+      userID: targetDeviceObj.userID
+    }
+    await Device.updateOne({ _id: deviceId }, newDeviceObj)
+
+    req.flash('success_msg', 'Your device has been deleted.')
+    res.redirect('/settings/admin_devices')
+  } catch (err) {
+    req.flash(
+      'error_msg',
+      'Oops. Something went wrong on our server. Please try again later' + err
+    )
+    res.redirect('/settings/admin_devices')
+  }
 })
 
 module.exports = router
