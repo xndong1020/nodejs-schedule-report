f01ff04b98ad51052d1b87a706aafbb7fccccc37
Jeremy Gu <isdance2004@hotmail.com>
2019-01-27 10:35:54 +1100

    add device edit page

--- routes/settings.js
+++ routes/settings.js
@@ -17,23 +17,63 @@ router.get('/add_device', async (req, res) => {
     key => key === settings.deviceType
   )
 
+  // available roles contains any unused roles
   let deviceRoleKeys = Object.keys(deviceRoles)
-
   deviceRoleKeys = deviceRoleKeys.filter(key => {
     return !deviceExitingRoles.includes(key.toLocaleLowerCase())
   })
+
   const currentDeviceRoleIndex = deviceRoleKeys.findIndex(
     key => key === settings.role
   )
 
   // read user settings
   res.render('add_device', {
-    title: 'Devices',
+    title: 'Add Device for Testing',
     name,
     settings,
     deviceKeys,
     currentDeviceIndex,
     deviceRoleKeys,
+    currentDeviceRoleIndex
+  })
+})
+
+// GET /settings/add_device
+router.get('/:deviceId/edit_device/:role', async (req, res) => {
+  const { _id } = req.user
+  const { deviceId, role } = req.params
+  const settings = (await getDeviceSettingsByUserID(_id)) || {}
+  const { devices } = settings
+  const deviceExitingRoles = devices.map(device => device.role)
+  const targetDevice = devices.find(device => device.role === role)
+  const { name } = req.user
+
+  const deviceKeys = Object.keys(deviceTypes)
+  const currentDeviceIndex = deviceKeys.findIndex(
+    key => key === settings.deviceType
+  )
+
+  // available roles contains any unused roles, plus current device role
+  let deviceRoleKeys = Object.keys(deviceRoles)
+  deviceRoleKeys = deviceRoleKeys.filter(key => {
+    return !deviceExitingRoles.includes(key.toLocaleLowerCase())
+  })
+  deviceRoleKeys.push(targetDevice.role.toUpperCase())
+  const currentDeviceRoleIndex = deviceRoleKeys.findIndex(
+    key => key.toLocaleLowerCase() === targetDevice.role.toLocaleLowerCase()
+  )
+
+  // read user settings
+  res.render('edit_device', {
+    title: 'Edit Device Details',
+    name,
+    deviceId,
+    settings: targetDevice,
+    deviceKeys,
+    currentDeviceIndex,
+    deviceRoleKeys,
+    currentDeviceRole: targetDevice.role,
     currentDeviceRoleIndex
   })
 })
@@ -80,7 +120,34 @@ router.post('/add_device', async (req, res) => {
   }
 })
 
-// POST /settings/add_device
+// POST /settings/5c37f750991eab536cb237f2/edit_device/primary
+router.post('/:deviceId/edit_device/:role', async (req, res) => {
+  const { deviceId, role } = req.params
+  try {
+    const targetDeviceObj = await Device.findOne({ _id: deviceId })
+    const { devices } = targetDeviceObj || []
+    let newDevicesList = devices
+    const targetDeviceIndex = devices.findIndex(device => device.role !== role)
+    newDevicesList[targetDeviceIndex] = { ...req.body }
+    const newDeviceObj = {
+      _id: targetDeviceObj._id,
+      devices: newDevicesList,
+      userID: targetDeviceObj.userID
+    }
+    await Device.updateOne({ _id: deviceId }, newDeviceObj)
+
+    req.flash('success_msg', 'Your device has been updated.')
+    res.redirect('/settings/admin_devices')
+  } catch (err) {
+    req.flash(
+      'error_msg',
+      'Oops. Something went wrong on our server. Please try again later: ' + err
+    )
+    res.redirect('/settings/admin_devices')
+  }
+})
+
+// POST /settings/5c37f750991eab536cb237f2/delete_device/primary
 router.post('/:deviceId/delete_device/:role', async (req, res) => {
   const { deviceId, role } = req.params
   try {
