9799e560555e1e8f149a3a794d2fe70cc72d8ecb
Jeremy Gu <isdance2004@hotmail.com>
2019-02-07 15:14:07 +1100

    add server side validation for creating task

--- enums/index.js
+++ enums/index.js
@@ -1,6 +1,6 @@
 const taskType = {
   CALL_STATUS: 'call_status',
-  CALL_HOLD_RESUME: 'hold_resume',
+  HOLD_RESUME: 'hold_resume',
   UNATTENDED_TRANSFER: 'unattended_transfer'
 }
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -7,6 +7,7 @@ const { Task } = require('../models/Task')
 const { taskType } = require('../enums')
 const { getDevicesList } = require('../utils/device.util')
 const { io } = require('../io')
+const { check, validationResult } = require('express-validator/check')
 
 router.get('/', async (req, res) => {
   res.render('calendar', { layout: 'layout_calendar' })
@@ -110,37 +111,117 @@ router.post('/edit/:taskId', async (req, res) => {
   }
 })
 
-router.post('/create', async (req, res) => {
-  const { _id } = req.user
-  const data = {
-    ...req.body,
-    start_date: DateTime.fromFormat(
-      req.body.start_date,
-      'MMMM d, yyyy'
-    ).toLocaleString(DateTime.DATE_FULL),
-    end_date: DateTime.fromFormat(
-      req.body.end_date,
-      'MMMM d, yyyy'
-    ).toLocaleString(DateTime.DATE_FULL),
-    userID: _id,
-    status: 'pending',
-    reportId: '',
-    completion_date: []
-  }
+router.post(
+  '/create',
+  [
+    check('text')
+      .not()
+      .isEmpty()
+      .withMessage('Please provide a valid task description'),
+    check('task_type')
+      .not()
+      .isEmpty()
+      .withMessage('Please select a valid task type'),
+    check('primary_device')
+      .not()
+      .isEmpty()
+      .withMessage('Please select a device for testing'),
+    check('secondary_device')
+      .not()
+      .isEmpty()
+      .withMessage('Please select a device for testing'),
+    check('third_device')
+      .not()
+      .isEmpty()
+      .withMessage('Please select a device for testing'),
+    check('start_date')
+      .not()
+      .isEmpty()
+      .withMessage('Please select a start date'),
+    check('end_date')
+      .not()
+      .isEmpty()
+      .withMessage('Please select an end date'),
+    check('run_at')
+      .not()
+      .isEmpty()
+      .withMessage('Please select start time')
+  ],
+  async (req, res) => {
+    const { _id } = req.user
+    const taskTypeKeys = Object.keys(taskType)
+    const taskTypeValues = Object.keys(taskType).map(key => {
+      return taskType[key]
+    })
+    const deviceNameList = await getDevicesList(_id)
+    const checkResult = validationResult(req)
+    const errors = checkResult.array()
 
-  try {
-    const newTask = await Task.create(data)
-    io.sockets.emit('newTask', newTask)
-    req.flash('success_msg', 'New task has been created successfully.')
-    res.redirect('/tasks/admin_tasks')
-  } catch (err) {
-    req.flash(
-      'error_msg',
-      'Oops. Something went wrong on our server. Please try again later' + err
-    )
-    res.redirect('/tasks/admin_tasks')
+    if (!checkResult.isEmpty()) {
+      // client-side validation failed
+      console.log(' req.body', req.body)
+      const {
+        text,
+        task_type,
+        primary_device,
+        secondary_device,
+        fourth_device,
+        third_device,
+        start_date,
+        end_date,
+        run_at
+      } = req.body
+      res.render('create_task', {
+        errors,
+        title: 'Create Task',
+        text,
+        task_type,
+        primary_device,
+        secondary_device,
+        third_device,
+        fourth_device,
+        start_date,
+        end_date,
+        run_at,
+        taskTypeKeys,
+        taskTypeValues,
+        deviceNameList,
+        error_msg: '',
+        success_msg: ''
+      })
+    } else {
+      const data = {
+        ...req.body,
+        start_date: DateTime.fromFormat(
+          req.body.start_date,
+          'MMMM d, yyyy'
+        ).toLocaleString(DateTime.DATE_FULL),
+        end_date: DateTime.fromFormat(
+          req.body.end_date,
+          'MMMM d, yyyy'
+        ).toLocaleString(DateTime.DATE_FULL),
+        userID: _id,
+        status: 'pending',
+        reportId: '',
+        completion_date: []
+      }
+
+      try {
+        const newTask = await Task.create(data)
+        io.sockets.emit('newTask', newTask)
+        req.flash('success_msg', 'New task has been created successfully.')
+        res.redirect('/tasks/admin_tasks')
+      } catch (err) {
+        req.flash(
+          'error_msg',
+          'Oops. Something went wrong on our server. Please try again later' +
+            err
+        )
+        res.redirect('/tasks/admin_tasks')
+      }
+    }
   }
-})
+)
 
 router.post('/delete/:taskId', async (req, res) => {
   const { taskId } = req.params
